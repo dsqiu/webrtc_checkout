1b094a869af568d68ba33a04c93a970c083a0c21
dsqiu <qdsfmt@126.com>
2017-07-03 21:45:19 +0800

    remove build

--- root/depot_tools.sh
+++ root/depot_tools.sh
@@ -3,5 +3,5 @@ export GYP_DEFINES="OS=linux"
 cd /root;git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 mkdir -p /root/webrtc-checkout
 cd /root/webrtc-checkout;fetch --nohooks webrtc_android;gclient sync
-cd /root/webrtc-checkout/src;./build/install-build-deps.sh
+#cd /root/webrtc-checkout/src;./build/install-build-deps.sh
 
