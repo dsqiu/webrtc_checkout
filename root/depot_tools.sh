65447a486247b329df87e638be990c8bda930d01
dsqiu <qdsfmt@126.com>
2017-07-03 20:28:24 +0800

    add path

--- root/depot_tools.sh
+++ root/depot_tools.sh
@@ -1,4 +1,4 @@
-export PATH=/root/depot_tools
+export PATH=/root/depot_tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 export GYP_DEFINES="OS=linux"
 cd /root;git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 mkdir -p /root/webrtc-checkout
