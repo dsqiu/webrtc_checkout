d66241d6711bb9d6b3bcd17b41943552fa989491
dsqiu <qdsfmt@126.com>
2017-07-03 22:40:39 +0800

    fix gclient

--- root/depot_tools.sh
+++ root/depot_tools.sh
@@ -1,7 +1,7 @@
 export PATH=/root/depot_tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 export GYP_DEFINES="OS=linux"
-cd /root;git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+cd /root && git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 mkdir -p /root/webrtc-checkout
-cd /root/webrtc-checkout;fetch --nohooks webrtc_android;gclient sync
-#cd /root/webrtc-checkout/src;./build/install-build-deps.sh
+cd /root/webrtc-checkout && fetch --nohooks webrtc_android && gclient sync -v --force
+cd /root/webrtc-checkout/src;./build/install-build-deps.sh
 
