0ed7baae9bec44e147ed4ddd14b13ea2d790b451
dsqiu <dongsheng.qiu@ingenic.com>
2017-07-05 12:43:58 +0800

    remote gclient sync

--- root/depot_tools.sh
+++ root/depot_tools.sh
@@ -2,6 +2,7 @@ export PATH=/root/depot_tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:
 export GYP_DEFINES="OS=linux"
 cd /root && git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 mkdir -p /root/webrtc-checkout
-cd /root/webrtc-checkout && fetch --nohooks webrtc_android && echo Y | gclient sync -v --force
+cd /root/webrtc-checkout && fetch --nohooks webrtc_android 
+#&& echo Y | gclient sync -v --force
 #cd /root/webrtc-checkout/src;./build/install-build-deps.sh
 
